378576951ca06bfa43ce5a6914692c7bdbb500ba
Jesus <sisniega.serrano@gmail.com>
2025-12-11 02:53:20 -0500

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,35 @@
+# public libraries #
+import numpy
+import time
+
+# personal scripts #
+import tomography2D
+import gfx
+
+####################
+
+# demo 1: First-steps into tomography
+tomography2D.demo(5, 3)
+
+# demo 2: Understanding the cube
+# gfx.visualizeMatrix([[[]]], "")
+
+# demo 3: 2D arrays visualized in 3D
+# temp = tomography2D.returnBoards(5, 3)
+# xray = temp[1]
+# gfx.visualizeMatrix(xray, "xray")
+# time.sleep(60)
+# mine = temp[0]
+# gfx.visualizeMatrix(mine, "mine")
+
+# demo 4: A 3D tomography solution
+# mine = [n for n in range(4)]
+# for r in mine:
+#     mine[r] = tomography2D.returnBoards(5, 3)[0]
+# gfx.visualizeMatrix(mine, "mine")
+
+# demo 5: Future work: 3D tomography problem generator
+# xray = [n for n in range(2)]
+# for r in xray:
+#     xray[r] = tomography2D.returnBoards(7, 1+2*r)[1]
+# gfx.visualizeMatrix(xray, "xray")
